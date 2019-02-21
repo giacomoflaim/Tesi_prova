a3a7aacef22575f55f29cb0684efc70b65a12950
Giacomo <giacomo.flaim.91@gmail.com>
2019-02-21 17:32:01 +0100

    filter-ok

---
+++ js/mio.js
@@ -0,0 +1,27 @@
+$(window).load(function(){
+    var $container = $('.portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.btn-2 a').click(function(){
+        $('.btn-2 .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
+});
