20d3a3a2c9e4ed7b79c01234c5ba35be17204f17
Giacomo <giacomo.flaim.91@gmail.com>
2019-04-09 20:59:42 +0200

    prova-csv

--- js/my-js.js
+++ js/my-js.js
@@ -38,20 +38,31 @@ $(document).ready(function(){
         window.history.pushState("object or string", "Tips", window.location.href.split("#")[0]);
         
     }
+    
+    
 });
 
 $('#btn-email').click(function(){
+    console.log("sono qui");
+    
     Email.send({
+        
         Host : "smtp.elasticemail.com",
         Username : "digital.driven.ideation.methods@gmail.com",
         Password : "a90cbf78-1bc9-4bea-bd3f-775e806cb430",
-        To : 'giacomo.flaim.91@gmail.com',
+        To : $('#exampleInputEmail1').val(),
         From : "digital.driven.ideation.methods@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-        message => alert(message)
-    );
+        Subject : "This is the real subject",
+        Body : "<h2>This is a reminder!</h4><br><h3>Check out <a href=\"www.ddim.it\">www.ddim.it<a> from your desktop device</h4>"
+        
+    }).then(function(message){
+        if(message === "OK")
+            $('#modal-email').modal('toggle')
+        else
+            console.log(message)
+    });
+   
+    
 });
 
 
